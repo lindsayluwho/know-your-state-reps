5c4a396cb20a2484e5976cac089991c93479ba2d
Lindsay Brown <lindsayluwho@gmail.com>
2017-12-26 21:09:11 -0500

    DOM manipulation for reps info card. need to slightly restructure code so that div is created before AJAX call so info is updated by AJAX call instead of creating a new card with each call. Also still need to add in committee info and make some styling tweaks to how card info displays.

--- assets/javascript/reps.js
+++ assets/javascript/reps.js
@@ -16,90 +16,146 @@ searchRep()
 //test
 
 //$("#search").on("click", function() {
-function searchRep(){
-	//event.preventDefault();
-	// var searchTerm = ($("#search-term").val())
-	// var totalResults = ($("#total-results").val())
-	// var startDate = ($("#start-date").val())
-	// var endDate = ($("#end-date").val())
+function searchRep() {
 
-	// startDate = startDate + '0101';
-	// endDate = endDate + '0101';
 
-	//searchTerm = "Linkin";
+    latitude = 40.960201;
+    longitude = -74.297324;
 
-	//startDate = "20010101";
-	//endDate = "20170101";
 
-	latitude = 40.960201;
-	longitude = -74.297324;
-	      
+    var queryURL = "https://openstates.org/api/v1/legislators/geo/?lat=" + latitude + "&long=" + longitude + "&apikey=8cef81cb-a1a4-48d3-86ff-0520d28f6ca6"
 
-	var queryURL = "https://openstates.org/api/v1/legislators/geo/?lat=" + latitude + "&long=" + longitude + "&apikey=8cef81cb-a1a4-48d3-86ff-0520d28f6ca6"
+    console.log("console log 1: " + queryURL);
 
-	console.log(queryURL);
-
-	$.ajax({
-	  url: queryURL,
-	  method: "GET"
-	})
-	.done(function(data) {
-//	  var results = data.response;
-	  //console.log(results);
-	  console.log(data);
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .done(function(data) {
+            //	  var results = data.response;
+            //console.log(results);
+            //console.log(data);
 
 
 
-	  for (var i = 0; i < data.length; i++) {
+            for (var i = 0; i < data.length; i++) {
 
 
-	  	var firstName = data[i].first_name;
-	  	var lastName = data[i].last_name;
+                var firstName = data[i].first_name;
+                var lastName = data[i].last_name;
 
-	  	//console.log(data[i].chamber);
+                //console.log(data[i].chamber);
 
-	  	if (data[i].chamber=="upper") {
-	  		if (!senatorLoaded) {
-	  		$("#senator").html(data[i].full_name + "<br>" + "Office: State Senator" + "<br>" + "District: " + data[i].district + "<br>"  );
+                if (data[i].chamber == "upper") {
+                    if (!senatorLoaded) {
+                        $("#senator").html(data[i].full_name + "<br>" + "Office: State Senator" + "<br>" + "District: " + data[i].district + "<br>");
+                        $("#senator").attr("data-id", data[i].leg_id);
+                        senatorLoaded = true;
+                        //console.log(data[i].full_name);
+                    }
+                }
 
-	  		senatorLoaded = true;
-	  		//console.log(data[i].full_name);
-	  		}
-	  	}
+                if (data[i].chamber == "lower") {
+                    if (!assembly1Loaded) {
+                        $("#assembly1").html(data[i].full_name + "<br>" + "Office: State Assembly" + "<br>" + "District: " + data[i].district + "<br>");
+                        $("#assembly1").attr("data-id", data[i].leg_id);
+                        assembly1Loaded = true;
+                        //console.log(data[i].full_name);
+                    } else {
+                        if (!assembly2Loaded) {
+                            $("#assembly2").html(data[i].full_name + "<br>" + "Office: State Assembly" + "<br>" + "District: " + data[i].district + "<br>");
+                            $("#assembly2").attr("data-id", data[i].leg_id);
+                            assembly2Loaded = true;
+                            //console.log(data[i].full_name);
+                        };
+                    }
+                }
 
-	  	if (data[i].chamber=="lower") {
-	  		if (!assembly1Loaded) {
-	  		$("#assembly1").html(data[i].full_name + "<br>" + "Office: State Assembly" + "<br>" + "District: " + data[i].district + "<br>" );
-
-	  		assembly1Loaded = true;
-	  		//console.log(data[i].full_name);
-	  		} else{
-  				if (!assembly2Loaded) {
-  					$("#assembly2").html(data[i].full_name + "<br>" + "Office: State Assembly" + "<br>" + "District: " + data[i].district + "<br>" );
-
-  					assembly2Loaded = true;
-  					//console.log(data[i].full_name);
-  					};
-	  		}
-	  	}	  	
-
-		  // var article = $("<div>");
-
-		  // article.addClass("article");
-
-		  // var headline = $("<div class='headline'>"+searchDocs[i].headline.main+"</div>");
-
- 
-		  // article.append(headline);
-		  // $(".col-xs-10").append(article);
-
-		  //console.log(firstName);
-		  //console.log(lastName);
-	  }
+            }
 
 
-	});	
-
-
+        });
 
 }
+
+$(".card-title").click(function() {
+
+    var legislatorID = $(this).attr("data-id");
+
+    //build the query URL and append the leg ID to it
+    var queryURL = "https://openstates.org/api/v1/legislators/" + legislatorID + "/?&apikey=8cef81cb-a1a4-48d3-86ff-0520d28f6ca6"
+    console.log(legislatorID);
+    console.log(queryURL);
+    // Creating an AJAX call for the specific card being clicked
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response) {
+        //      	console.log(response.length)
+        // for (var i = 0; i < response.length; i++) {
+
+        console.log("printing card");
+
+        var infoBox = $("<div id='info-box' class='row'>");
+        var infoCol = $("<div id='info-col' class='col s12'>");
+
+        var idCard = $("<div id='id-card' class='card blue-grey darken-1'>");
+        var cardBlue = $("<div class='card-content white-text'>");
+
+
+        var cardRow = $("<div class='row'>");
+        var nameClicked = $("<div id='name-clicked' class='col s8'>");
+        var photoCol = $("<div id='photo-column' class='col s4'>");
+        var titleRow = $("<div class='row'>");
+        var titleSpan = $("<span id='name-clicked' class='card-title'>");
+
+        var legName = $("<p class='card-content white-text'>").text("Name : " + response.full_name);
+        var party = $("<p class='card-content white-text'>").text("Party: " + response.party);
+        var email = $("<p class='card-content white-text'>").text("Email: " + response.email);
+        
+        var photoURL = response.photo_url;
+
+        var photo = $("<img src='"+ photoURL + "' class='card-content'>");
+        var offices = response.offices;
+        var phone = $("<p class='card-content white-text'>").text("Phone: " + offices[0].phone);
+        var address = $("<p class='card-content white-text'>").text("Office Address: "+ offices[0].address);
+        // append all the sections
+
+
+        // $(".section").append(myDIV);
+
+
+        $(".section").append(infoBox);
+        $("#info-box").append(infoCol);
+        $("#info-col").append(idCard);
+
+
+
+        infoCol.append(idCard);
+        idCard.append(cardBlue);
+        cardBlue.append(cardRow);
+        cardRow.append(nameClicked);
+        cardRow.append(photoCol);
+        photoCol.append(photo);
+        nameClicked.append(legName);
+        nameClicked.append(party);
+        nameClicked.append(address);
+        nameClicked.append(phone);
+        nameClicked.append(email);
+
+        infoBox.append(infoCol);
+
+        // $("#info-box").append(infoCol)
+
+
+
+        // append to the main
+
+
+        console.log(response);
+
+        // }
+    });
+
+
+});
